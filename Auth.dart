59f12784475a8b97a6843038ed232442757187f5
xenomech <gokulsmenon227@gmail.com>
2020-08-02 13:52:37 +0530

    create a funtion to sign in with email and password!

--- Auth.dart
+++ Auth.dart
@@ -1,3 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthService {}
+class AuthService {
+  // sign in with email and password
+  Future signinwithEmailandPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _fromfirebaseuser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
