d22b467578a4ea1e1074d4d820172234219a2187
xenomech <gokulsmenon227@gmail.com>
2020-08-02 13:59:21 +0530

    adding a few things i missed out !

---
+++ models/user.dart
@@ -0,0 +1,5 @@
+class User {
+  final String uid;
+  final String name;
+  User({this.uid, this.name});
+}
--- services/Auth.dart
+++ services/Auth.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:________________/Models/user.dart'; // fill in with your app name
 
 class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance; //private variable _auth
+
   // creating a user model based on firebase user
   User _fromfirebaseuser(FirebaseUser user) {
     return user != null ? User(uid: user.uid, name: user.displayName) : null;
